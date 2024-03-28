6dbadc559a0e0f57f7ec340aaedbb9ba92b277c2
h20170047 <46812751+h20170047@users.noreply.github.com>
2024-03-28 17:27:04 +0530

    Adding identity service and authentication

--- src/main/java/com/svj/config/EcomGatewayConfig.java
+++ src/main/java/com/svj/config/EcomGatewayConfig.java
@@ -1,5 +1,8 @@
 package com.svj.config;
 
+import com.svj.filter.AuthenticationFilter;
+import jakarta.validation.constraints.AssertFalse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -7,20 +10,26 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class EcomGatewayConfig {
+//    @Autowired
+//    private AuthenticationFilter authenticationFilter;
 
-    @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder){
-        return builder.routes()
-                .route("oder-service", r->r.path("/orders/**")
-                        .and()
-                        .uri("lb://ORDER-SERVICE"))
-                .route("payment-service", r->r.path("/payments/**")
-                        .and()
-                        .uri("lb://PAYMENT-SERVICE"))
-                .route("user-service", r->r.path("/users/**")
-                        .and()
-                        .uri("lb://USER-SERVICE"))
-
-                .build();
-    }
+//    @Bean
+//    public RouteLocator routeLocator(RouteLocatorBuilder builder){
+//        return builder.routes()
+//                .route("oder-service", r->r.path("/orders/**")
+////                        .filters(f->f.filter(authenticationFilter.apply(new Object())))
+//                        .and()
+//                        .uri("lb://ORDER-SERVICE"))
+//                .route("payment-service", r->r.path("/payments/**")
+//                        .and()
+//                        .uri("lb://PAYMENT-SERVICE"))
+//                .route("user-service", r->r.path("/users/**")
+//                        .and()
+//                        .uri("lb://USER-SERVICE"))
+//                .route("identity-service", r->r.path("/auth/**")
+//                        .and()
+//                        .uri("lb://IDENTITY-SERVICE"))
+//
+//                .build();
+//    }
 }
